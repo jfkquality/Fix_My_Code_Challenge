74c2c62c5bad2fecb921d471e5936cd77946fbd4
jfkquality <jfkquality@gmail.com>
2020-02-27 06:09:23 +0000

    0x01-challenge. Task 1. Fix square.py.

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
 
-class square():
-    
+class Square():
+    """Square class (more like rectangle)."""
     width = 0
     height = 0
 
-    
+
     def __init__(self, *args, **kwargs):
+        """init values"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
+
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
-    def PermiterOfMySquare(self):
+
+    def permiter_of_my_square(self):
+        """Perimeter of square/rectangle"""
         return (self.width * 2) + (self.height * 2)
+
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
